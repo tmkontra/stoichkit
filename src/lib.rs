fefd053700d384282352cce02806b5c6de1fd3a7
Tyler Kontra <tyler@tylerkontra.com>
2020-07-24 11:25:48 -0700

    chore(release): 0.2.5 - remove blas, ndarray dangling deps

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
-extern crate blas;
 extern crate env_logger;
 #[macro_use]
 extern crate log;
-extern crate ndarray;
-extern crate ndarray_linalg;
-extern crate openblas_src;
 
 pub mod model;
 pub mod molecule;
