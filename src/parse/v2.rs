38303ef5a3257736e2962b7f9c0b5dd0d1a5ad6e
Tyler Kontra <tyler@tylerkontra.com>
2021-10-13 21:54:12 -0700

    clean up parser

--- src/parse/v2.rs
+++ src/parse/v2.rs
@@ -1,9 +1,7 @@
-use crate::model::Element;
-use itertools::fold;
 use std::collections::hash_map::RandomState;
 use std::collections::HashMap;
 
-use crate::parse::element_from_string;
+use itertools::fold;
 use nom::branch::alt;
 use nom::bytes::complete::{take_until, take_while, take_while1};
 use nom::character::complete::digit1;
@@ -22,72 +20,88 @@ use nom::{
 };
 use periodic_table_on_an_enum::Element::Hydrogen;
 
-fn sym2(sym: &str) -> IResult<&str, String> {
-    map(
-        pair(
-            take_while_m_n(1, 1, |c: char| c.to_owned().is_ascii_uppercase()),
-            opt(take_while1(|c: char| c.to_owned().is_ascii_lowercase())),
+use crate::model::Element;
+use crate::parse::element_from_string;
+
+/// Parse an elemental symbol, e.g. H, He, Na, S, Co
+fn symbol(sym: &str) -> IResult<&str, Element> {
+    map_res(
+        map(
+            // one capital letter and possibly a second lower-case letter
+            pair(
+                take_while_m_n(1, 1, |c: char| {
+                    c.to_owned().is_ascii_uppercase()
+                }),
+                opt(take_while1(|c: char| c.to_owned().is_ascii_lowercase())),
+            ),
+            |(one, two): (&str, Option<&str>)| {
+                format!("{}{}", one, two.unwrap_or(""))
+            },
         ),
-        |(one, two): (&str, Option<&str>)| {
-            format!("{}{}", one, two.unwrap_or(""))
-        },
+        |s| element_from_string(&s),
     )(sym)
 }
 
-fn element_from_String(string: String) -> Result<Element, String> {
-    element_from_string(&string)
-}
-
-fn sym(sym: &str) -> IResult<&str, Element> {
-    map_res(sym2, element_from_String)(sym)
-}
-
-fn mult(mult: &str) -> IResult<&str, u64> {
+/// Parse the multiplier following a symbol, must be an integer (1 or more digits)
+fn multiplier(mult: &str) -> IResult<&str, u64> {
     map_res(digit1, |s: &str| s.parse::<u64>())(mult)
 }
 
-fn elem(elem: &str) -> IResult<&str, (Element, Option<u64>)> {
-    pair(sym, opt(mult))(elem)
+/// Parse an element with its multiplier
+fn element(elem: &str) -> IResult<&str, (Element, u64)> {
+    // no multiplier means 1
+    pair(symbol, opt(multiplier).map(|m| m.unwrap_or(1)))(elem)
 }
 
-fn elems_to_map(vec: Vec<(Element, Option<u64>)>) -> HashMap<Element, u64> {
-    vec.into_iter()
-        .map(|(el, maybeMult)| (el, maybeMult.unwrap_or(1)))
-        .collect()
+/// Sums a vector of elements and their multipliers,
+/// rolling them into a map
+fn sum_elements(vec: Vec<(Element, u64)>) -> HashMap<Element, u64> {
+    vec.into_iter().collect()
 }
 
+/// Parses a group, which is a sequence of elements, each optionally
+/// followed by a multiplier, e.g. COOH or CO2H or NaCl2
 fn group(group: &str) -> IResult<&str, HashMap<Element, u64>> {
-    map(many1(elem), elems_to_map)(group)
+    map(many1(element), sum_elements)(group)
 }
 
+/// Parses a multi-group, which is a parenthetical group
+/// optionally followed by a multiplier, e.g. (SO4) or (SO4)2
 fn multi_group(multi_group: &str) -> IResult<&str, HashMap<Element, u64>> {
     map(
-        pair(delimited(tag("("), group, tag(")")), opt(mult)),
-        |(g, m)| {
-            let mx: u64 = m.unwrap_or(1);
-            g.to_owned()
-                .iter()
-                .fold(HashMap::new(), |mut acc, (el, num)| {
+        pair(
+            delimited(tag("("), group, tag(")")),
+            opt(multiplier).map(|m| m.unwrap_or(1)),
+        ),
+        |(group, multiplier)| {
+            group.to_owned().iter().fold(
+                HashMap::new(),
+                |mut acc, (el, num)| {
                     let entry: &mut u64 = acc.entry(el.to_owned()).or_insert(0);
-                    *entry += num * mx;
+                    *entry += num * multiplier;
                     acc
-                })
+                },
+            )
         },
     )(multi_group)
 }
 
+/// Parses an asterisk followed by "H2O"
 fn hydrate(hydrate: &str) -> IResult<&str, HashMap<Element, u64>> {
     let (hydrate, _) = tag("*")(hydrate)?;
-    map(pair(opt(mult), tag("H2O")), |(maybeMult, _H2O)| {
-        let mx = maybeMult.unwrap_or(1);
-        let mut map = HashMap::new();
-        map.insert(Element::from_symbol("H").unwrap(), 2 * mx);
-        map.insert(Element::from_symbol("O").unwrap(), mx);
-        map
-    })(hydrate)
+    map(
+        pair(opt(multiplier).map(|m| m.unwrap_or(1)), tag("H2O")),
+        |(mx, _hydrate)| {
+            let mut map = HashMap::new();
+            map.insert(Element::from_symbol("H").unwrap(), 2 * mx);
+            map.insert(Element::from_symbol("O").unwrap(), mx);
+            map
+        },
+    )(hydrate)
 }
 
-fn fold_maps(vec: Vec<HashMap<Element, u64>>) -> HashMap<Element, u64> {
+/// Sums a vector of group results
+fn sum_groups(vec: Vec<HashMap<Element, u64>>) -> HashMap<Element, u64> {
     vec.into_iter().fold(HashMap::new(), |mut acc, m| {
         for (el, mul) in m {
             let c = acc.entry(el).or_insert(0);
@@ -97,34 +111,45 @@ fn fold_maps(vec: Vec<HashMap<Element, u64>>) -> HashMap<Element, u64> {
     })
 }
 
-fn formula_parser(formula: &str) -> IResult<&str, Vec<HashMap<Element, u64>>> {
+/// Parses the full formula
+/// Must contain at least one group or multi-group, e.g. H2O or (SO4)2
+/// May also have a "hydrate" suffix, e.g. *6H2O
+fn formula_parser(formula: &str) -> IResult<&str, HashMap<Element, u64>> {
     map(
-        pair(many1(alt((multi_group, group))), opt(hydrate)),
-        |(groups, maybeHydrate)| match &maybeHydrate {
-            None => groups,
-            Some(hydrate) => {
-                let mut g = groups.to_owned();
-                let mut h = vec![hydrate.to_owned()].to_owned();
-                g.append(h.as_mut());
-                g
-            }
-        },
+        map(
+            pair(many1(alt((multi_group, group))), opt(hydrate)),
+            |(groups, maybeHydrate)| match &maybeHydrate {
+                None => groups,
+                Some(hydrate) => {
+                    let mut hydrate_group = groups.to_owned();
+                    let mut h = vec![hydrate.to_owned()].to_owned();
+                    hydrate_group.append(h.as_mut());
+                    hydrate_group
+                }
+            },
+        ),
+        sum_groups,
     )(formula)
 }
 
+/// Parse a compound formula string.
+/// # Examples
+/// parse_formula_v2("H2O")
+/// parse_formula_v2("H2(SO4)2")
 pub fn parse_formula_v2(
     formula: &str,
 ) -> Result<HashMap<Element, u64>, String> {
     let (rem, elems) =
         formula_parser(formula).map_err(|_e| "failed!".to_string())?;
-    Ok(fold_maps(elems))
+    Ok(elems)
 }
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
     use crate::model::Element;
     use crate::parse::v2::parse_formula_v2;
-    use std::collections::HashMap;
 
     #[test]
     fn test_H2O() {
